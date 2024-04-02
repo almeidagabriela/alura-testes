817ce136a88c17a9e964c6660ee1b8c6b99a5508
Gabriela Almeida <gabrielabrazolin@outlook.com>
2024-04-02 10:54:29 -0300

    criando testes de excecoes

--- alura.estacionamento/Alura.Estacionamento.Testes/VeiculoTest.cs
+++ alura.estacionamento/Alura.Estacionamento.Testes/VeiculoTest.cs
@@ -61,6 +61,33 @@ namespace Alura.Estacionamento.Testes
             Assert.Contains("Ficha do Veículo:", dados);
         }
 
+        [Fact]
+        public void NomeProprietarioVeiculoComMenosDeTresCaracteres()
+        {
+            // Arrange
+            string nomeProprietario = "Ab";
+
+            // Act + Assert
+            Assert.Throws<System.FormatException>(
+                () => new Veiculo(nomeProprietario)
+            );
+        }
+
+        [Fact]
+        public void MensagemDeExcecaoDoQuartoCaractereDaPlaca()
+        {
+            // Arrange
+            string placa = "ASDF8888";
+
+            // Act
+            var mensagem = Assert.Throws<System.FormatException>(
+                    () => new Veiculo().Placa = placa
+                );
+
+            // Assert
+            Assert.Equal("O 4° caractere deve ser um hífen", mensagem.Message);
+        }
+
         public void Dispose()
         {
             SaidaConsoleTeste.WriteLine("Dispose invocado.");
